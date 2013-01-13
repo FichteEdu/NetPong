13b4059b37f246f57e5ae819ef11f647018633fd
FichteFoll <fichtefoll2@googlemail.com>
2013-01-13 17:06:14 +0100

    Reorganize Interface imports

--- src/Interface.java
+++ src/Interface.java
@@ -1,8 +1,7 @@
 import java.awt.*;
 import java.awt.geom.Rectangle2D;
-//import java.awt.event.*;
-import javax.swing.*;
-//import javax.swing.event.*;
+import javax.swing.JFrame;
+import javax.swing.WindowConstants;
 
 /**
   * ToDoc
