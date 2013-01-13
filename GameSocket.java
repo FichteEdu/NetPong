70258e95f3c2fc57862eee602f3530d35c51c1cf
Alexander <a.buddenbrock@ish.de>
2013-01-13 14:43:51 +0100

    Communicator
    Threads ftw

--- GameSocket.java
+++ GameSocket.java
@@ -2,12 +2,12 @@ import java.io.*;
 import java.net.*;
 import java.util.Random;
 
-public class GameSocket {
+public class GameSocket implements Runnable {
 
     private Socket socket;
     private ServerSocket listener;
     private String host;
-    private int port = 11000;
+    private int port = 1100;
 
     private PrintWriter out;
     private BufferedReader in;
@@ -15,20 +15,63 @@ public class GameSocket {
     private short lastPos = 0;
     static Random r = new Random();
 
-    GameSocket(String host) {
-        this.host = host;
+    GameSocket() throws IOException {
+        listener = new ServerSocket(this.port);
+        socket = new Socket();
+
     }
 
-    // TODO: wtfisthisidonteven
-    // 
-    public void connect() throws IOException {
-        listener = new ServerSocket(this.port);
-        socket = new Socket(this.host, this.port);
-        //waiting for accept
-        Socket tmp = listener.accept();
-        out = new PrintWriter(socket.getOutputStream(), true);
-        in = new BufferedReader(new InputStreamReader(tmp.getInputStream()));
+    
+    public static void main(String[] args) throws IOException, InterruptedException {
+        GameSocket s = new GameSocket();
+        s.connect("localhost");
+        while(!s.isConnected()) {
+            System.out.println("Waiting for connection");
+            Thread.sleep(100);
+        }
+        while(true) {
+            int a = r.nextInt(8);
+            System.out.println("Writing: " + a);
+            s.writePosition((short)a);
+            System.out.println("Got: " + s.getPosition());
+            Thread.sleep(100);
+        }
     }
+
+
+    
+    public void connect(String host) throws IOException {
+        this.host = host;
+        new Thread(this).start();
+    }
+
+
+    public boolean isConnected() {
+        return socket.isConnected();
+    }
+
+
+    public void run() {
+        try {
+            while(true) {
+                try {
+                    socket.connect(new InetSocketAddress(this.host, this.port));
+                } catch (ConnectException e) {
+                    Thread.sleep(1000);
+                    continue;
+                }
+                
+                System.out.println("Timeout: " + socket.getSoTimeout());
+                //waiting for accept
+                Socket tmp = listener.accept();
+                out = new PrintWriter(socket.getOutputStream(), true);
+                in = new BufferedReader(new InputStreamReader(tmp.getInputStream())); 
+            }
+        } catch (Exception e) {
+
+        }
+    }
+
 
     public void writePosition(short pos) {
         out.write(pos);
