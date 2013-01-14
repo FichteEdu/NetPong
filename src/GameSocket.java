cdff23ac079c067abff305a74052c523e63688a1
Alexander <a.buddenbrock@ish.de>
2013-01-14 21:47:19 +0100

    Minor changes to the communicator
    Added comments
    Added function to cancel connection attempt
    Added ball collision
    No longer throws exceptions

--- src/GameSocket.java
+++ src/GameSocket.java
@@ -1,6 +1,7 @@
 import java.io.*;
 import java.net.*;
 import java.util.Random;
+import java.nio.ByteBuffer;
 
 public class GameSocket implements Runnable {
 
@@ -9,10 +10,12 @@ public class GameSocket implements Runnable {
     private String host;
     private int port = 1100;
 
-    private PrintWriter out;
-    private BufferedReader in;
+    private OutputStream out;
+    private InputStream in;
 
-    private short lastPos = 0;
+    private int lastPos = 0;
+    private boolean hasCollided = false;
+    private boolean isCancelled = false;
     static Random r = new Random();
 
     GameSocket() throws IOException {
@@ -22,6 +25,7 @@ public class GameSocket implements Runnable {
     }
 
     
+    // for testing purposes only
     public static void main(String[] args) throws IOException, InterruptedException {
         GameSocket s = new GameSocket();
         s.connect("localhost");
@@ -32,40 +36,42 @@ public class GameSocket implements Runnable {
         while(true) {
             int a = r.nextInt(8);
             System.out.println("Writing: " + a);
-            s.writePosition((short)a);
+            s.writePosition(a, true);
             System.out.println("Got: " + s.getPosition());
             Thread.sleep(100);
         }
     }
 
 
-    
-    public void connect(String host) throws IOException {
+    // attempts to connect to the host
+    // does not block until a connection is established
+    public void connect(String host) {
+        isCancelled = false;
         this.host = host;
         new Thread(this).start();
     }
 
 
+    // returns true if the socket is connected
     public boolean isConnected() {
         return socket.isConnected();
     }
 
 
+    // does the actual connecting
     public void run() {
         try {
-            while(true) {
+            while(!isCancelled) {
                 try {
                     socket.connect(new InetSocketAddress(this.host, this.port));
                 } catch (ConnectException e) {
                     Thread.sleep(1000);
                     continue;
                 }
-                
-                System.out.println("Timeout: " + socket.getSoTimeout());
                 //waiting for accept
                 Socket tmp = listener.accept();
-                out = new PrintWriter(socket.getOutputStream(), true);
-                in = new BufferedReader(new InputStreamReader(tmp.getInputStream())); 
+                out = socket.getOutputStream();
+                in = tmp.getInputStream();
             }
         } catch (Exception e) {
 
@@ -73,25 +79,64 @@ public class GameSocket implements Runnable {
     }
 
 
-    public void writePosition(short pos) {
-        out.write(pos);
-        out.flush();
+
+    // cancels the current connection attempt
+    public void cancel() {
+        isCancelled = true;
     }
 
-    public short getPosition() throws IOException {
-        if (in.ready()) {
-            short pos = (short)in.read();
-            if (pos == -1) return lastPos;
-            return (lastPos = pos);
-        } else {
-            return lastPos;
+
+    // writes the position of the player's bar and whether the ball has collided with it in the stream and flushes it
+    public void writePosition(int pos, boolean hasCollided) {
+        try {
+            out.write(ByteBuffer.allocate(4).putInt(hasCollided ? pos  | Integer.MIN_VALUE : pos).array());
+            out.flush();
+        } catch (IOException e) {
+            // will probably never happen
         }
     }
 
-    public void disconnect() throws IOException {
-        in.close();
-        out.close();
-        socket.close();
+
+    // returns the current position of the other player's bar
+    public int getPosition() {
+        try {
+            while(in.available() >= 4) {
+                byte[] b = new byte[4];
+                if(in.read(b) < 4)
+                    return lastPos;
+                ByteBuffer pos = ByteBuffer.wrap(b);
+                // lastPos = pos.getInt() & 0xF;
+                lastPos = pos.getInt();
+                hasCollided = lastPos < 0;
+                if(hasCollided)
+                    lastPos &= 0xF;
+            }
+        } catch (IOException e) {
+            return lastPos;
+        }
+        return lastPos;
+    }
+
+
+    // returns whether or not the ball has collided with the other player's bar
+    public boolean hasCollided() {
+        if(hasCollided) {
+            hasCollided = false;
+            return true;
+        }
+        return hasCollided;
+    }
+
+
+    // disconnects
+    public void disconnect() {
+        try {
+            in.close();
+            out.close();
+            socket.close();
+        } catch (IOException e) {
+
+        }
     }
 
 }
